780f3294e98c7091b3eebf316c04fdd86b251a2e
nckPro1 <kien2862004@gmail.com>
2026-03-27 00:54:17 +0700

    Initial commit: Setup Spring Boot, PostgreSQL, Maven dependencies and core Entities

---
+++ src/main/java/org/example/nihongobackend/NihongoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.nihongobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NihongoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NihongoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nihongobackend/NihongoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nihongobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NihongoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
